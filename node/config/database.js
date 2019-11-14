5db2104aaa2ce1d1b9912bd3152634d1374dee2d
ujjaval-barot <56751657+ujjaval-barot@users.noreply.github.com>
2019-11-14 23:34:09 +0530

    Update database.js

--- node/config/database.js
+++ node/config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongourl: `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0-7wbjv.mongodb.net/test?retryWrites=true&w=majority`
+  mongourl: `mongodb+srv://ujjaval:Ujjaval@123@cluster0-7wbjv.mongodb.net/test?retryWrites=true&w=majority`
 };
